8fe8abba5a0fd0e0b7a52ab85aaada8f72b26841
guilherme <guilhermeborgesti@gmail.com>
2018-11-18 10:08:08 -0200

    refatoracao

--- src/main/java/br/com/api/utils/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.utils;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
