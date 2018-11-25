25feb588e2e94e8db116480fca05e13a21711beb
guilherme <guilhermeborgesti@gmail.com>
2018-11-25 13:07:21 -0200

    refactoring project

--- src/main/java/br/com/api/repositories/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.entity.LoginEntity;
-
-public interface LoginRepository extends JpaRepository<LoginEntity, Long>{}
